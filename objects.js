0b0517f4f6d96d117058492ff6d4a33fbb39f7f4
Ian Davall <iduality5n@gmail.com>
2019-01-10 18:19:10 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ var updatePlaylist = function(playlist, artistName, songTitle) {
   return playlist;
 }
 
-var removeFromPlaylist = function(playlist, artistName, songTitle) {
+var removeFromPlaylist = function(playlist, artistName) {
   playlist[artistName] = songTitle;
   return playlist;
 }
